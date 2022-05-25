434ae4df086514556c0dde25a3d755c03310837d
Sharan-99 <sharan.ceg2017103591@gmail.com>
2022-05-25 09:21:05 +0530

    first commit

---
+++ CodeAnalysisinVS_Using_RoslynAnalyzer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CodeAnalysisinVS_Using_RoslynAnalyzer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a N value to print");
+            int n = int.Parse(Console.ReadLine());
+            int i = 0;
+            while(i<n)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+        }
+    }
+}
